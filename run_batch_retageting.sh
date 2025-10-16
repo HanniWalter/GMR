13264b53de55624873ae87440cb15d6244eac7a1
Johannes Walter <hannibali@gmx.de>
2025-10-16 14:37:53 +0200

    update custom script

--- run_batch_retageting.sh
+++ run_batch_retageting.sh
@@ -2,7 +2,7 @@
 
 robot="booster_k1"
 output_dir="/home/nao/Documents/GMR/output/${robot}"
-
+num_cpus=22
 # Clean output directory if it exists
 if [ -d "$output_dir" ]; then
     echo "Cleaning existing output directory..."
@@ -21,23 +21,17 @@ conda env remove -n gmr -y
 
 # create conda env
 conda create -n gmr python=3.10 -y
+
+# Activate the environment
 conda activate gmr
 
 # install GMR
-conda run -n gmr pip install -e .
+pip install -e .
 
 # to resolve some possible rendering issues
-conda install -n gmr -c conda-forge libstdcxx-ng -y
+conda install -c conda-forge libstdcxx-ng -y
 
-echo ""
-echo "=========================================="
-echo "Starting retargeting process..."
-echo "=========================================="
-echo ""
+# Activate the environment directly instead of using conda run
+python scripts/smplx_to_robot_dataset.py --src_folder "/home/nao/Documents/GMR/assets/body_models/AMASS_-x_npz" --tgt_folder "$output_dir/training_data/" --robot "$robot" --num_cpus "$num_cpus"
 
-conda run -n gmr python scripts/smplx_to_robot_dataset.py --src_folder "/home/nao/Documents/GMR/assets/body_models/AMASS_-x_npz" --tgt_folder "$output_dir/training_data/" --robot "$robot" --num_cpus 22
 
-echo ""
-echo "=========================================="
-echo "Retargeting process completed!"
-echo "=========================================="
